10a4d5f0bd28b5a78d579d3fa7466e77e59ce0c5
Grace <gracemuig18@gmail.com>
2023-02-21 12:50:27 +0300

    innit

--- buy products/contact.php
+++ buy products/contact.php
@@ -8,6 +8,28 @@ if(isset($_SESSION['user_id'])){
     $user_id = $_SESSION['user_id'];
 }else{
     $user_id = '';
+};
+
+if(isset($_POST['send'])){
+    $name = $_POST['name'];
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $email = $_POST['email'];
+    $email = filter_var($email, FILTER_SANITIZE_STRING);
+    $number = $_POST['number'];
+    $number = filter_var($number, FILTER_SANITIZE_STRING);
+    $msg = $_POST['msg'];
+    $msg = filter_var($msg, FILTER_SANITIZE_STRING);
+
+    $select_message = $conn->prepare("SELECT * FROM `messages` WHERE name = ? AND email = ? AND number = ? AND message = ?");
+    $select_message->execute([$name, $email, $number, $msg]);
+
+    if($select_message->rowCount() > 0){
+        $message[] = 'message sent already';
+    }else{
+        $send_message = $conn->prepare("INSERT INTO `messages`(name, email, number, message) VALUES(?,?,?,?)");
+        $send_message->execute([$name, $email, $number, $msg]);
+        $message[] = 'successfully sent!';
+    }
 }
 
 ?>
@@ -40,6 +62,24 @@ if(isset($_SESSION['user_id'])){
 </head>
 <body>
 <?php include 'components/user_header.php'?>
+
+<!-- contact section starts -->
+
+<section class="form-container">
+
+    <h1 class="heading">Contact Us</h1>
+
+    <form method="post" action="" class="box">
+        <h3>Send A Message</h3>
+        <input type="text" name="name" required placeholder="enter your name" maxlength="20" class="box">
+        <input type="number" name="number" required placeholder="enter your phone number" max="9999999999" min="0" class="box" onkeypress="if(this.value.length == 10) return false;">
+        <input type="email" name="email" required placeholder="enter your email" maxlength="50" class="box">
+        <textarea name="msg" placeholder="enter your message" class="box" cols="30" rows="10"></textarea>
+        <input type="submit" value="send message" class="btn" name="send">
+    </form>
+</section>
+
+<!-- contact section ends -->
 
 
 
