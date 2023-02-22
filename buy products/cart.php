d61e4f458626b55cfc801aab962a94b0745ab893
Grace <gracemuig18@gmail.com>
2023-02-23 02:10:41 +0300

    innit

--- buy products/cart.php
+++ buy products/cart.php
@@ -8,7 +8,29 @@ if(isset($_SESSION['user_id'])){
     $user_id = $_SESSION['user_id'];
 }else{
     $user_id = '';
-    header('location:site_products.php');
+    header('location:user_login.php');
+}
+
+if(isset($_POST['delete'])){
+    $cart_id = $_POST['cart_id'];
+    $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE id = ?");
+    $delete_cart->execute([$cart_id]);
+    $message[] = 'Removed from cart!';
+}
+
+if(isset($_GET['delete_id'])){
+    $delete_all = $_GET['delete_all'];
+    $delete_all_cart = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+    $delete_all_cart->execute([$user_id]);
+    header('location:cart.php');
+}
+
+if(isset($_POST['update_qty'])){
+    $cart_id = $_POST['cart_id'];
+    $qty = $_POST['qty'];
+    $update_qty = $conn->prepare("UPDATE `cart` SET quantity = ? WHERE id = ?");
+    $update_qty->execute([$qty, $cart_id]);
+    $message[] = 'cart quantity updated successfully!';
 }
 
 ?>
@@ -41,6 +63,56 @@ if(isset($_SESSION['user_id'])){
 </head>
 <body>
 <?php include 'components/user_header.php'?>
+
+<!-- cart section starts -->
+
+<section class="products">
+
+    <h1 class="heading">shopping cart</h1>
+
+    <div class="box-container">
+
+        <?php
+            $grand_total = 0;
+            $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+            $select_cart->execute([$user_id]);
+            if($select_cart->rowCount() > 0){
+                while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
+        ?>
+
+        <form action="" method="post" class="box">
+            <input type="hidden" name="cart_id" value="<?= $fetch_cart['id']; ?>">
+            <a href="quick_view.php?pid=<?= $fetch_cart['pid']; ?>" class="fas fa-eye"></a>
+            <img src="hair_products_img/<?= $fetch_cart['image']; ?>" class="image" alt="">
+            <div class="name"><?= $fetch_cart['name']; ?></div>
+            <div class="flex">
+                <div class="price">Ksh<span><?= $fetch_cart['price']; ?></span>/=</div>
+                <input type="number" name="qty" class="qty" min="1" max="99" value="<?= $fetch_cart['quantity']; ?>" onkeypress="if(this.value.length == 2) return false;">
+                <button type="submit" class="fas fa-edit" name="update_qty"></button>
+            </div>
+            <div class="sub-total">sub total:  <span>Ksh<?= $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?>/=</span></div>
+                <input type="submit" value="remove item" onclick="return confirm('Remove this product from cart?');" name="delete" class="delete-btn">
+        </form>
+
+        <?php
+            $grand_total += $sub_total;
+                }
+            }else{
+                echo '<p class="empty">your cart is empty</p>';
+            }
+        ?>
+    </div>
+    <div class="grand-total">
+
+            <p>grand total:  <span>Ksh<?= $grand_total ?>/=</span></p>
+            <a href="shop.php" class="option-btn">Continue Shopping</a>
+            <a href="cart.php?delete_all" class="delete-btn <?= ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('Remove all products from cart?');">Remove All</a>
+            <a href="checkout.php" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>">Checkout</a>
+
+    </div>
+</section>
+
+<!-- cart section ends -->
 
 
 
--- buy products/favourites.php
+++ buy products/favourites.php
@@ -13,6 +13,20 @@ if(isset($_SESSION['user_id'])){
 
 include 'components/favourites_cart.php';
 
+if(isset($_POST['delete'])){
+    $favourites_id = $_POST['favourites_id'];
+    $delete_favourites = $conn->prepare("DELETE FROM `favourites` WHERE id = ?");
+    $delete_favourites->execute([$favourites_id]);
+    $message[] = 'Removed from favourites!';
+}
+
+if(isset($_GET['delete_id'])){
+    $delete_all = $_GET['delete_all'];
+    $delete_all_favourites = $conn->prepare("DELETE FROM `favourites` WHERE user_id = ?");
+    $delete_all_favourites->execute([$user_id]);
+    header('location:favourites.php');
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -53,15 +67,20 @@ include 'components/favourites_cart.php';
     <div class="box-container">
 
         <?php
+            $grand_total = 0;
             $select_favourites = $conn->prepare("SELECT * FROM `favourites` WHERE user_id = ?");
             $select_favourites->execute([$user_id]);
             if($select_favourites->rowCount() > 0){
                 while($fetch_favourites = $select_favourites->fetch(PDO::FETCH_ASSOC)){
-  
+                    $grand_total += $fetch_favourites['price'];
         ?>
 
         <form action="" method="post" class="box">
-            <input type="hidden" name="">
+            <input type="hidden" name="pid" value="<?= $fetch_favourites['pid']; ?>">
+            <input type="hidden" name="name" value="<?= $fetch_favourites['name']; ?>">
+            <input type="hidden" name="price" value="<?= $fetch_favourites['price']; ?>">
+            <input type="hidden" name="image" value="<?= $fetch_favourites['image']; ?>">
+            <input type="hidden" name="favourites_id" value="<?= $fetch_favourites['id']; ?>">
             <a href="quick_view.php?pid=<?= $fetch_favourites['pid']; ?>" class="fas fa-eye"></a>
             <img src="hair_products_img/<?= $fetch_favourites['image']; ?>" class="image" alt="">
             <div class="name"><?= $fetch_favourites['name']; ?></div>
@@ -70,7 +89,7 @@ include 'components/favourites_cart.php';
                 <input type="number" name="qty" class="qty" min="1" max="99" value="1" onkeypress="if(this.value.length == 2) return false;">
             </div>
             <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-                <input type="submit" value="delete item" onclick="return confirm('delete this product from favourites?');" name="delete" class="delete-btn">
+                <input type="submit" value="remove item" onclick="return confirm('Remove this product from favourites?');" name="delete" class="delete-btn">
         </form>
 
         <?php
@@ -79,6 +98,13 @@ include 'components/favourites_cart.php';
                 echo '<p class="empty">your favourites is empty</p>';
             }
         ?>
+    </div>
+    <div class="grand-total">
+
+            <p>grand total:  <span>Ksh<?= $grand_total ?>/=</span></p>
+            <a href="shop.php" class="option-btn">Continue Shopping</a>
+            <a href="favourites.php?delete_all" class="delete-btn <?= ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('Remove all products from favourites?');">Remove All</a>
+
     </div>
 </section>
 
